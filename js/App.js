6bb3b70c68c80cdbc71a9cc39f5c93e9c1957098
SaraOsacar <saraosacar@gmail.com>
2023-10-02 11:02:12 +0200

    variables & giveaway

--- js/App.js
+++ js/App.js
@@ -24,3 +24,30 @@ const weekdays = [
   'Friday',
   'Saturday',
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+console.log(items);
+
+
+let futureDate = new Date(2025, 2, 26, 13,30, 0);
+
+
+const year = futureDate.getFullYear();
+/*const month = futureDate.getMounth();*/
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes(); 
+
+let month = futureDate.getMonth();
+month = month[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays [futureDate.getDay()];
+//console.log(weekday);
+
+
+giveaway.textContent = `Tickets sales end on ${weekday} ${month} ${year} ${hours}: ${minutes}pm`;
+
+const futureTime = futureDate.getTime();
+console.log(futureTime);
