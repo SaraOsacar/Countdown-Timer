c3481b8750e15d61ab2bf952167968cabb086aab
SaraOsacar <saraosacar@gmail.com>
2023-10-02 15:48:17 +0200

    Function get remaining time

--- js/App.js
+++ js/App.js
@@ -25,29 +25,54 @@ const weekdays = [
   'Saturday',
 ];
 
-const giveaway = document.querySelector(".giveaway");
-const deadline = document.querySelector(".deadline");
-const items = document.querySelectorAll(".deadline-format h4");
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
 console.log(items);
 
-
-let futureDate = new Date(2025, 2, 26, 13,30, 0);
-
+let futureDate = new Date(2025, 1, 26, 13, 30, 0);
 
 const year = futureDate.getFullYear();
-/*const month = futureDate.getMounth();*/
+const monthName = months[futureDate.getMonth()];
 const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes(); 
-
-let month = futureDate.getMonth();
-month = month[month];
+const minutes = futureDate.getMinutes();
 const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
 
-const weekday = weekdays [futureDate.getDay()];
-//console.log(weekday);
-
-
-giveaway.textContent = `Tickets sales end on ${weekday} ${month} ${year} ${hours}: ${minutes}pm`;
+giveaway.textContent = `Tickets sales end on ${weekday}, ${date} ${monthName} ${year} ${hours}:${minutes}pm`;
 
 const futureTime = futureDate.getTime();
-console.log(futureTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // Cálculo de todos los valores
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
+
+setInterval(getRemainingTime, 1000);
